5611515a731baa292043301d94ffca27a920e927
Sreejith Thrivikraman <mail.sreejith.23@gmail.com>
2018-08-28 23:29:40 -0400

    Add files via upload

--- focus/source.js
+++ focus/source.js
@@ -15,7 +15,7 @@ function auto()
 function backgr()
 {
     var backimg = ["images/pic.jpg","images/pic2.jpeg","images/pic3.jpeg"];
-    var randimg =Math.floor(Math.random()*3);
+    var randimg = Math.floor(Math.random()*3);
     document.body.background = backimg[randimg];
     setInterval(backgr,30000);
 }
@@ -63,7 +63,8 @@ function check_key()
       {
         task = $("input#focuses").val();
         localStorage.setItem("task_name1",task);
-        HTML_content = "<ul> <li> "+ task +"  </li></ul>";
+
+        HTML_content = "<h2 id='focuses' style='display:inline;border:none;font-family: Arial, Helvetica, sans-serif;'>"+ task +"  </h2>"
         $("#focuses").replaceWith(HTML_content);
         $("#hidden").show();
         $("#hidden_chk").show();
@@ -184,11 +185,13 @@ function initialize()
         if((loaded_task == "")||(loaded_task==null))
         {
           $("#focuses").val('');
-          $(hidden_chk).hide();
+         $(hidden_chk).hide();
         }
         else
         {
-          HTML_content = "<ul> <li> "+ loaded_task +"  </li></ul>";
+          HTML_content = "<h2 id='focuses' style='display:inline;border:none;font-family: Arial, Helvetica, sans-serif;'>"+ loaded_task +"  </h2>";
+
+
           $("#focuses").replaceWith(HTML_content);
           $("#hidden").show();
           $("#hidden_chk").show();
@@ -203,15 +206,16 @@ function checkbox_change()
   if(document.getElementById('hidden_chk').checked)
   {
    // checkBox is checked.
-   HTML_content = "<ul> <s><li> "+ task +"  </li></s></ul>";
-   $("ul:first").replaceWith(HTML_content);
+   // HTML_content = "<h2 id= 'focuses'> <s>"+ task +"  </s></h2>";
+   HTML_content = "<h2 id='focuses' style='display:inline;font-family: Arial, Helvetica, sans-serif;border:none'> <s>"+ task +"</s></h2>";
+   $("#focuses").replaceWith(HTML_content);
 
   }
   else
   {
    // Not checked.
-   HTML_content = "<ul><li> "+ task +"  </li></ul>";
-   $("ul:first").replaceWith(HTML_content);
+   HTML_content = "<h2 id='focuses' style='display:inline;font-family: Arial, Helvetica, sans-serif;border:none'>"+ task +"</h2>";
+   $("#focuses").replaceWith(HTML_content);
   }
 }
 
@@ -221,9 +225,16 @@ function pop_up_checkbox_change(id)
   if(document.getElementById('pop_chk').checked)
   {
    // checkBox is checked.
-   HTML_content = '<li> <span id='+id+'><input id="pop_chk" type="checkbox" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> '+item_list+' <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span></li>';
-   $('#'+id ).replaceWith(HTML_content);
 
+   HTML_content = '<li> <span id="+id+"><input id="pop_chk" type="checkbox" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> <h2><s>'+item_list+'</s> </h2> <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span></li>';
+
+   //$(ul_pop_up).append('<li> <span id='+todo_item_no+'><input id="pop_chk" type="checkbox" style="display:inline;" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> <div id="text" style="display:inline;">'+item_list+'</div> <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span></li>');
+
+
+
+   var id = $('#'+id).closest('span').attr('id');
+
+   $('#'+id).replaceWith(HTML_content);
   }
   else
   {
@@ -242,11 +253,13 @@ function delete_task()
   var userName = localStorage.getItem("focus_name_temp_4");
   localStorage.setItem("task_name1","");
   $("#user_name").replaceWith(userName);
-  $("ul:first").replaceWith('<input id="focuses" type="text" onkeypress="check_key()"></input>');
+  $("#focuses").replaceWith("<input id='focuses' type='text' onkeypress='check_key()''></input>");
 
 
 }
 
+
+// todo - list
 function pop_up_logic()
 {
   if(popped_up_flag == true)
@@ -269,7 +282,10 @@ function check_key_pop()
     if(event.keyCode === 13)
     {
       var item_list = $("input#pop_up_input").val();
-      $(ul_pop_up).append('<li> <span id='+todo_item_no+'><input id="pop_chk" type="checkbox" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> <div id="text">'+item_list+'</div> <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span></li>');
+       $(ul_pop_up).append('<li> <span id='+todo_item_no+'><input id="pop_chk" type="checkbox" style="display:inline;" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> <div id="text" style="display:inline;">'+item_list+'</div> <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span></li>');
+      var li = document.createElement("list");
+    //  li.appendChild('<span id='+todo_item_no+'><input id="pop_chk" type="checkbox" style="display:inline;" onchange="pop_up_checkbox_change(this.parentNode.id)"></input> <div id="text" style="display:inline;">'+item_list+'</div> <button id="bingo" type="button" onclick="popup_delete_task(this.parentNode.id)">X</button></span>');
+    //  $(ul_pop_up).appendChild(li);
       $("#pop_up_input").val('');
       todo_item_no = todo_item_no + 1;
     }
